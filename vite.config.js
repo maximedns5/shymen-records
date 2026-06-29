4f6fd92a96a4ecea3468a8dd771de74f789e4ed5
Maxime Denis <maximedns92@gmail.com>
2026-06-29 15:03:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shymen-records/',
+  base: '/',
   server: { port: 5175, strictPort: true },
 })
